4933e3308e121f33faf9296d423af2ad9ac8c712
fEgor <fegor1818@icloud.com>
2025-08-01 16:23:24 +0200

    Initial commit

---
+++ MyTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTracker
+//
+//  Created by Admin on 01.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
